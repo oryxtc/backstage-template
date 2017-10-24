4a078744afdd16caf3444be250cef94f8a68a560
oryxtc <oryxtc@gmail.com>
2017-10-24 15:02:41 +0800

    新增获取 菜单接口 新增中间件 允许跨域

---
+++ database/migrations/2017_10_24_093331_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('parent_id')->default(0)->comment('父级id');
+            $table->string('menu_name','255')->comment('菜单名称');
+            $table->unsignedInteger('sorting')->default(0)->comment('排序值');
+            $table->timestamps();
+            $table->index('sorting');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
